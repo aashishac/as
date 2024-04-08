378eeb0c9a7764b0c0528b624ac89957280802d4
aashishac <127003356+aashishac@users.noreply.github.com>
2024-04-08 19:26:08 +0545

    new commit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SelectField
 from passlib.hash import sha256_crypt
 from functools import wraps
-from flask_uploads import UploadSet, configure_uploads, IMAGES
+#from flask_uploads import UploadSet, configure_uploads, IMAGES
 import timeit
 
 from flask import redirect, url_for, session
@@ -62,8 +62,8 @@ import functools
 app = Flask(__name__, static_folder='static')
 app.secret_key = os.urandom(24)
 app.config['UPLOADED_PHOTOS_DEST'] = 'static/image/product'
-photos = UploadSet('photos', IMAGES)
-configure_uploads(app, photos)
+#photos = UploadSet('photos', IMAGES)
+#configure_uploads(app, photos)
 
 app.secret_key = '8gBm/:&EnhH.1/q'
 # Config MySQL
